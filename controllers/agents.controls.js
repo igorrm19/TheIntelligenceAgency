2de43afed8e09289404e0edaf052a63f3a95175c
igorrm23 <igormachadofire14@gmail.com>
2026-01-22 06:38:08 -0300

    adicionando rota de obter por id

--- controllers/agents.controls.js
+++ controllers/agents.controls.js
@@ -1,9 +1,38 @@
 const Models = require("../models/agents.models")
+const fs = require('fs');
 
-exports.getAgents = async (req, res) => {
+exports.getAgents = async (req, res) => {  // funcionando
     try {
         const agents = await Models.getFile();
         res.status(200).json(agents);
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ error: "Um erro aconteceu" })
+    }
+}
+
+exports.getIdAgents = async (req, res) => {  //funcionando
+    try {
+        const agents = await fs.promises.readFile("agents.json", "utf-8");
+        const agent = JSON.parse(agents).find((agent) => String(agent.id) === req.params.id);
+
+        if (!agent) {
+            return res.status(404).send({ message: "Agente não encontrado" });
+        }
+
+        res.send(agent);
+
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ error: "Um erro aconteceu" })
+    }
+}
+
+exports.createIdAgents = async (req, res) => {
+    try {
+
 
     } catch (err) {
         console.log(err)
@@ -11,19 +40,21 @@ exports.getAgents = async (req, res) => {
     }
 }
 
-exports.getIdAgents = async (req, res) => {
-    console.log("ok");
-}
-
-exports.createIdAgents = async (req, res) => {
-    console.log("ok");
-}
-
 exports.updateIdAgents = async (req, res) => {
-    console.log("ok");
+    try {
+
+    } catch (err) {
+        console.log(err)
+        res.status(404).send({ error: "Um erro aconteceu" })
+    }
 }
 
 exports.deleteIdAgents = async (req, res) => {
-    console.log("ok");
+    try {
+
+    } catch (err) {
+        console.log(err)
+        res.status(404).send({ error: "Um erro aconteceu" })
+    }
 }
 
