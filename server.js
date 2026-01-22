77b9213799ea35b5ecfaeaaec19e5e197ccf04f2
igorrm23 <igormachadofire14@gmail.com>
2026-01-22 04:03:54 -0300

    configurando porta

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-console.log("Hello World");
+const express = require("express");
+const port = 3000;
+
+const app = express();
+app.use(express.json());
 
 
-
-
-
+app.listen(port || 3001, () => {
+    console.log("Servidor rodando na porta", port);
+});
